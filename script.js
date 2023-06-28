7f9ecbf2075bd973e4f07a2763e13a884a339d8f
herbertis <62161409+herbertis@users.noreply.github.com>
2023-06-27 21:37:17 -0300

    vs01

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle("light")
+  html.classList.toggle("dark")
 
   // pegar a tag img
   const img = document.querySelector("#profile img")
 
   // substituir a imagem
-  if (html.classList.contains("light")) {
+  if (html.classList.contains("dark")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/eu.jpg")
   } else {
     // set tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/eu.jpg")
   }
 }
